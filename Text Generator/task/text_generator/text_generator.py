1b6d3a3c3c2338594a08700728b628d6db00bd4a
fvn70 <fvn_reg@mail.ru>
2022-02-14 22:40:58 +0700

    new Stage 5/6

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -2,6 +2,35 @@ import random
 from nltk.tokenize import WhitespaceTokenizer
 from collections import Counter
 
+def is_first(word):
+    return word[0] == word[0].capitalize() and not is_eol(word)
+
+def is_last(word):
+    return word[-1] in ".?!"
+
+def is_eol(word):
+    return word[-1] in ".?!"
+
+
+def read_word(head, type):
+    if not head:
+        head = random.choice(list(dic.keys()))
+    tails = dic[head].most_common(20)
+    random.shuffle(tails)
+    for t in tails:
+        word = t[0]
+        if type == 'first' and is_first(word):
+            return word
+        if type == 'middle' and not is_eol(word):
+            return word
+        if type == 'last':
+            return word
+    if type == 'first':
+        return read_word('', type)
+    else:
+        return ''
+
+
 # fn = "../test/corpus.txt"
 fn = input()
 text = ""
@@ -24,13 +53,26 @@ for k, v in bigrams:
 
 dic = {k: Counter(dic[k]) for k in dic}
 
-for _ in range(10):
-    head = random.choice(bigrams)[0]
-    line = head
-    for _ in range(9):
-        tails = dic[head].most_common(10)
-        tail = random.choice(tails)[0]
-        line += ' ' + tail
-        head = tail
-    print(line)
-
+j = 0
+while j < 10:
+    j += 1
+    line = ''
+    word = ''
+    loop = True
+    while loop:
+        word = read_word(word, 'first')
+        line += word + ' '
+        while len(line.split()) < 5:
+            word = read_word(word, 'middle')
+            if word:
+                line += word + ' '
+            else:
+                loop = False
+                j -= 1
+                break
+        while loop and not is_eol(word):
+            word = read_word(word, 'last')
+            line += word + ' '
+        break
+    if loop:
+        print(line)
