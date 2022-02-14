1c5e1521544e4bd3a75d75de9a2b015484c3fd41
fvn70 <fvn_reg@mail.ru>
2022-02-14 19:13:49 +0700

    new Stage 4/6

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -1,3 +1,4 @@
+import random
 from nltk.tokenize import WhitespaceTokenizer
 from collections import Counter
 
@@ -23,17 +24,13 @@ for k, v in bigrams:
 
 dic = {k: Counter(dic[k]) for k in dic}
 
-while True:
-    cmd = input('\n')
-    if cmd == "exit":
-        break
-    try:
-        head = cmd
-        print(f"Head: {head}")
-        tails = dic[head].most_common()
-        for t in tails:
-            print(f"Tail: {t[0]} Count: {t[1]}")
+for _ in range(10):
+    head = random.choice(bigrams)[0]
+    line = head
+    for _ in range(9):
+        tails = dic[head].most_common(10)
+        tail = random.choice(tails)[0]
+        line += ' ' + tail
+        head = tail
+    print(line)
 
-    except KeyError:
-        msg = "Key Error. The requested word is not in the model. Please input another word."
-        print(msg)
