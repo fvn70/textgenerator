60ee8695f1bee557d1951afd68a2ff2b96960ebc
fvn70 <fvn_reg@mail.ru>
2022-02-15 09:21:13 +0700

    new Stage 6/6

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -1,15 +1,17 @@
 import random
+import string
+
 from nltk.tokenize import WhitespaceTokenizer
 from collections import Counter
 
 def is_first(word):
-    return word[0] == word[0].capitalize() and not is_eol(word)
+    return word[0][0] in string.ascii_uppercase and not is_last(word) and not is_eol(word)
 
 def is_last(word):
-    return word[-1] in ".?!"
+    return word.split()[0][-1] in ".?!"
 
 def is_eol(word):
-    return word[-1] in ".?!"
+    return word.split()[-1][-1] in ".?!"
 
 
 def read_word(head, type):
@@ -18,7 +20,7 @@ def read_word(head, type):
     tails = dic[head].most_common(20)
     random.shuffle(tails)
     for t in tails:
-        word = t[0]
+        word = head.split()[1] + ' ' + t[0]
         if type == 'first' and is_first(word):
             return word
         if type == 'middle' and not is_eol(word):
@@ -43,12 +45,12 @@ except FileNotFoundError:
 tk = WhitespaceTokenizer()
 tokens = tk.tokenize(text)
 
-bigrams = []
-for i in range(len(tokens) - 1):
-    bigrams.append((tokens[i], tokens[i+1]))
+trigrams = []
+for i in range(len(tokens) - 2):
+    trigrams.append((tokens[i] + ' ' + tokens[i+1], tokens[i+2]))
 
 dic = {}
-for k, v in bigrams:
+for k, v in trigrams:
     dic.setdefault(k, []).append(v)
 
 dic = {k: Counter(dic[k]) for k in dic}
@@ -65,14 +67,14 @@ while j < 10:
         while len(line.split()) < 5:
             word = read_word(word, 'middle')
             if word:
-                line += word + ' '
+                line += word.split()[1] + ' '
             else:
                 loop = False
                 j -= 1
                 break
         while loop and not is_eol(word):
             word = read_word(word, 'last')
-            line += word + ' '
+            line += word.split()[1] + ' '
         break
     if loop:
         print(line)
